572e9953d5fe551ee2bde7102b3d504a9415f234
Anna <annaf656@students.cs.ubc.ca>
2022-11-21 15:50:11 -0800

    Added test classes for Event and EventLog

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -3,39 +3,27 @@ package model;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * Represents an alarm system event.
- */
+//REFERENCE LIST: the following code mimics behaviour seen in AlarmSystem project provided in CPSC 210,
+// which can be found at https://github.students.cs.ubc.ca/CPSC210/AlarmSystem.git/.
+
+//Represents a shopping cart or shopper event.
 public class Event {
     private static final int HASH_CONSTANT = 13;
     private Date dateLogged;
     private String description;
 
-    /**
-     * Creates an event with the given description
-     * and the current date/time stamp.
-     *
-     * @param description a description of the event
-     */
+    //Creates an event with the given description and the current date/time stamp
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
         this.description = description;
     }
 
-    /**
-     * Gets the date of this event (includes time).
-     *
-     * @return the date of the event
-     */
+    //Gets the date of this event (includes time).
     public Date getDate() {
         return dateLogged;
     }
 
-    /**
-     * Gets the description of this event.
-     *
-     * @return the description of the event
-     */
+    // Gets the description of this event.
     public String getDescription() {
         return description;
     }
